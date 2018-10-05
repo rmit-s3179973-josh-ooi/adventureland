f1a1953b59bcaaaac37cbc94f30df1cbf9a7ec56
Josh Ooi <josh.ronald.ooi@gmail.com>
2018-10-05 12:01:06 +1000

    - integrated laravel/passport package - migrated user and passport database - created UserController to handle register and login - tested function works

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laravel\Passport\PassportServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -220,8 +220,7 @@ return [
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-
+        'View' => Illuminate\Support\Facades\View::class,        
     ],
 
 ];
